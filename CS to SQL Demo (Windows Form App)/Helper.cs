8c0c9de444a44a5b96184ae38fb387fb48d69b3e
David Dimmen <David.Dimmen@bluemargin.com>
2021-12-24 07:21:45 -0700

    Commit 2

---
+++ CS to SQL Demo (Windows Form App)/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_to_SQL_Demo__Windows_Form_App_
+{
+    public static class Helper
+    {
+        public static string ConnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
