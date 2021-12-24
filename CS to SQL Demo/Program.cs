f7d3a4a74dcdc7d1bdb4ca4fd0ccbb8ae14d48a5
David Dimmen <David.Dimmen@bluemargin.com>
2021-12-24 06:50:46 -0700

    Create Project

---
+++ CS to SQL Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_to_SQL_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
